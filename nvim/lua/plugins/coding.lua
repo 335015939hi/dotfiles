9b3efedcf01ab87c784576018626ea76e30ea81b
Tony Zhou <tonyzhou2916@gmail.com>
2026-09-01 17:28:43 -0400

    nvim: shift+tab next autocomplete, ctrl+tab prev
    Signed-off-by: Tony Zhou <tonyzhou2916@gmail.com>

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -8,8 +8,8 @@ return {
         preset = "default",
 
         ["<Tab>"] = { "accept", "fallback" },
-        ["<C-Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<S-Tab>"] = { "select_next", "fallback" },
+        ["<C-Tab>"] = { "select_prev", "fallback" },
 
         ["<Up>"] = { "fallback" },
         ["<Down>"] = { "fallback" },
