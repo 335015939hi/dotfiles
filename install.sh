916ee7519a338fbb22e3556128e8b64ba7eb1d56
Tony Zhou <tonyzhou2916@gmail.com>
2026-08-31 20:28:16 -0400

    add stuff
    aliases
    error message on install fail
    zsh-autosuggestions submodule
    
    Signed-off-by: Tony Zhou <tonyzhou2916@gmail.com>

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ cd "$(dirname "$0")" || exit 2
 DIR="$PWD"
 
 install() {
-  mkdir -p "$(dirname "$HOME/$2")"
-  ln -s "$DIR/$1" "$HOME/$2"
+  NEWDIR="$(dirname "$HOME/$2")"
+  mkdir -p "$NEWDIR" || echo "mkdir '$NEWDIR' failed ($?)"
+  ln -s "$DIR/$1" "$HOME/$2" || echo "install $1 to $2 failed ($?)"
 }
 
 install zshrc .zshrc
 install profile .profile
+install aliases .aliases
