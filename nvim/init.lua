ecf9a4657d67472b7266466d98fe891e809df88c
Tony Zhou <tonyzhou2916@gmail.com>
2026-09-12 11:17:17 -0400

    nvim/matugen: new custom matugen template for nvim
    Signed-off-by: Tony Zhou <tonyzhou2916@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,16 +8,13 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 
-local normal = vim.api.nvim_get_hl(0, { name = "Normal", link = false })
-
-vim.api.nvim_set_hl(0, "CursorLine", {
-  bg = normal.bg + 0x111111,
-})
-
 vim.cmd("colorscheme matugen")
 vim.api.nvim_create_autocmd("Signal", {
   pattern = "SIGUSR1",
-  command = "colorscheme matugen",
+  callback = function()
+    vim.cmd("colorscheme matugen")
+    require("lualine").setup()
+  end,
 })
 
 --vim.cmd([[
