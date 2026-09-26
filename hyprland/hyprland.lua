cc96a994c612baf1cd8aeefdd94dee7b60e39dba
Tony Zhou <tonyzhou2916@gmail.com>
2026-09-26 10:30:32 -0400

    hyprland: basic configs
    Signed-off-by: Tony Zhou <tonyzhou2916@gmail.com>

--- hyprland/hyprland.lua
+++ hyprland/hyprland.lua
@@ -1,1 +1,18 @@
+-- hyprmods forces mouse actions (move, resize) only for mouse buttons, so we must declare this here
+hl.bind("SUPER + mouse:276", hl.dsp.window.move({ workspace = "+1" }))
+hl.bind("SUPER + mouse:275", hl.dsp.window.move({ workspace = "-1" }))
+
+-- neither does hyprmod allow advanced stuff like this
+hl.bind("SUPER + Z", function()
+	local active_window = hl.get_active_window()
+	if not active_window then
+		return
+	end
+	if string.match(active_window.workspace.name, "special") then
+		hl.dispatch(hl.dsp.window.move({ workspace = "+0" }))
+	else
+		hl.dispatch(hl.dsp.window.move({ workspace = "special", follow = false }))
+	end
+end)
+
 require("hyprland-gui")
