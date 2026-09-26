ddf1e75aeffa037a41f0803529d3c19d8a13b525
Tony Zhou <tonyzhou2916@gmail.com>
2026-09-26 11:43:05 -0400

    hyprland: add walker (app launcher)
    Signed-off-by: Tony Zhou <tonyzhou2916@gmail.com>

--- hyprland.sh
+++ hyprland.sh
@@ -3,3 +3,19 @@ link hyprland .config/hypr
 
 # hyprmod, a hyprland configuration GUI
 (cd "$DIR/hyprmod" && ./install.sh)
+
+# elephant, a walker dependency
+function elephant_install() {
+  if checkcmd elephant >/dev/null; then
+    return 0
+  fi
+  requirecmd go
+  cd "$DIR/elephant" || return 1
+  cd cmd/elephant || return 1
+  export GOBIN=$HOME/.local/bin
+  go install elephant.go || return 1
+}
+(elephant_install)
+
+# walker, a launcher
+checkcmd walker >/dev/null || (cd walker && cargo install --path .)
